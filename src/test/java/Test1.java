ad1b5497a772fad4a6df9f257f315b2e65f13d14
ellaliviev <lelalou100@gmail.com>
2024-02-27 11:16:09 +0200

    initial commit

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Test1 {
+import org.example.Main;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void test1(){
+        System.out.println(Main.five());
+    }
+
 }
